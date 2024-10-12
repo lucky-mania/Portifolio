7af5f68e7cffddd1e73490b445d7d39abb85d8c5
lucky-mania <joeaneto@gmail.com>
2024-10-12 15:52:16 -0300

    007

---
+++ 007/007.js
@@ -0,0 +1,11 @@
+function play() {
+    var tn1 = window.document.getElementById('txtn1');
+    var res = window.document.getElementById('res');
+    if(tn1.value == 'o verdadeiro não esta entre eles'){
+        res.innerHTML ='<a href="https://youtu.be/6NGayWHlG6o?si=JmwxiA7wPoYwEqkc" target="_blank" class="boxi">play</a>'
+        res.style.background='blue'
+    }else{
+        res.innerHTML ='errado'
+        res.style.background='red'
+    }
+}
